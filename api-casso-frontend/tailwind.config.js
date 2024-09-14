be6a1ad02c91b0a8c882129e86917c8b897730fd
Karolina Dubiel <62366293+dubiels@users.noreply.github.com>
2024-09-13 23:22:20 -0700

    Update tailwind.config.js

--- api-casso-frontend/tailwind.config.js
+++ api-casso-frontend/tailwind.config.js
@@ -8,7 +8,9 @@ module.exports = {
   theme: {
     extend: {
       fontFamily: {
-        sans: ['Inter', 'sans-serif'], 
+        sans: ['var(--font-inter)', 'sans-serif'], // Inter as the default sans
+        geistSans: ['var(--font-geist-sans)', 'sans-serif'],
+        geistMono: ['var(--font-geist-mono)', 'monospace'],
       },
       colors: {
         background: "var(--background)",
