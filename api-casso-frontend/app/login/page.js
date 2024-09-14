f5eaff9c44479151c35301edc707f24270e4b8e7
Karolina Dubiel <62366293+dubiels@users.noreply.github.com>
2024-09-14 07:49:33 -0700

    adding login page

---
+++ api-casso-frontend/app/login/page.js
@@ -0,0 +1,32 @@
+export default function Signup() {
+    return (
+  
+      
+      <div className="min-h-screen flex flex-col items-center justify-center bg-gray-900 text-white">
+  
+        {/* Top Header */}
+  
+  
+        <h1 className="text-4xl font-bold mb-8">Welcome back 👋</h1>
+        <form className="flex flex-col space-y-4 w-80">
+          <input
+            type="email"
+            placeholder="Email Address"
+            className="px-4 py-2 rounded bg-gray-800 text-white border border-gray-600"
+          />
+          <input
+            type="password"
+            placeholder="Password"
+            className="px-4 py-2 rounded bg-gray-800 text-white border border-gray-600"
+          />
+          <button
+            type="submit"
+            className="px-4 py-2 bg-black rounded-lg text-white font-semibold hover:bg-gray-700"
+          >
+            Login
+          </button>
+        </form>
+      </div>
+    );
+  }
+  
