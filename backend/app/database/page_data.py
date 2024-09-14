51573518cef1c34f4d1f4c381e13ebbc1acb5d91
KevinL <kkevin.li@mail.utoronto.ca>
2024-09-14 16:04:18 -0400

    Change functions to Flask APIs

--- backend/app/database/page_data.py
+++ backend/app/database/page_data.py
@@ -1,19 +1,28 @@
 from supabase import create_client, Client
 import constants, query_constants
 from typing import List
+from run import app
+from flask import Flask, request, jsonify
 
 url: str = constants.SUPABASE_URL
 key: str = constants.SUPABASE_KEY
 
 supabase: Client = create_client(url, key)
 
-def getPageDataByWebsite(website_url:str, user_id:str) -> str:
+@app.route('/data/website', methods=['GET'])
+def getPageDataByWebsite() -> str:
+  data = request.json  # Get the JSON data sent in the request
+  website_url = data["website_url"]
+  user_id = data["user_id"]
   response = supabase.table(query_constants.PAGE_DATA_TABLE).select("*").eq(query_constants.WEBSITE_URL_COLUMN, website_url).eq(query_constants.USER_ID_COLUMN, user_id).execute()
   print(response)
   pageData = response[0][query_constants.PAGE_DATA_COLUMN]
   return pageData
 
-def getPageDataForUser(user_id:str) -> List[str]:
+@app.route('/data/user', methods=['GET'])
+def getPageDataForUser() -> List[str]:
+  data = request.json  # Get the JSON data sent in the request
+  user_id = data["user_id"]
   response = supabase.table(query_constants.PAGE_DATA_TABLE).select("*").eq("user_id", user_id).execute()
   pageData = []
 
@@ -23,7 +32,12 @@ def getPageDataForUser(user_id:str) -> List[str]:
 
   return pageData
 
-def insertPageData(website_url:str, user_id:str, page_data:str) -> bool:
+@app.route('/data/insert', methods=['POST'])
+def insertPageData() -> bool:
+  data = request.json  # Get the JSON data sent in the request
+  website_url = data["website_url"]
+  user_id = data["user_id"]
+  page_data = data["page_data"]
   try:
     response = supabase.table(query_constants.PAGE_DATA_TABLE).insert({query_constants.WEBSITE_URL_COLUMN:website_url, query_constants.PAGE_DATA_COLUMN: page_data, query_constants.USER_ID_COLUMN:user_id}).execute()
     print(response)
@@ -34,4 +48,4 @@ def insertPageData(website_url:str, user_id:str, page_data:str) -> bool:
 
 # insertPageData("walmart.com", "kevin", "new data")
 # insertPageData("bestbuy.com", "kevin", "new data")
-getPageDataForUser("kevin")
+# getPageDataForUser("kevin")
--- backend/app/database/schemas.py
+++ backend/app/database/schemas.py
@@ -1,19 +1,30 @@
 from supabase import create_client, Client
 import json
 import constants, query_constants
+from flask import Flask, request, jsonify
+from run import app
 
 url: str = constants.SUPABASE_URL
 key: str = constants.SUPABASE_KEY
 
 supabase: Client = create_client(url, key)
 
-def getSchemaByWebsiteAndUser(website_url:str, user_id:str) -> dict:
+@app.route('/schema/website', methods=['GET'])
+def getSchemaByWebsiteAndUser() -> dict:
+  data = request.json  # Get the JSON data sent in the request
+  website_url = data["website_url"]
+  user_id = data["user_id"]
   response = supabase.table(query_constants.RESPONSE_SCHEMA_TABLE).select("*").eq(query_constants.WEBSITE_URL_COLUMN, website_url).eq(query_constants.USER_ID_COLUMN, user_id).execute()
   json = response.data[0][query_constants.RESPONSE_SCHEMA_COLUMN]
   print(json["cheese"])
   return json
 
-def insertSchema(website_url:str, user_id:str, schema_string:str) -> bool:
+@app.route('/schema/insert', methods=['POST'])
+def insertSchema() -> bool:
+  data = request.json  # Get the JSON data sent in the request
+  website_url = data["website_url"]
+  user_id = data["user_id"]
+  schema_string = data["schema_string"]
   try:
     schema = json.loads(schema_string)
     response = supabase.table(query_constants.RESPONSE_SCHEMA_TABLE).insert({query_constants.WEBSITE_URL_COLUMN:website_url, query_constants.USER_ID_COLUMN:user_id, query_constants.RESPONSE_SCHEMA_COLUMN:schema}).execute()
@@ -23,11 +34,10 @@ def insertSchema(website_url:str, user_id:str, schema_string:str) -> bool:
     print(e)
     return False
   
-  
   return True
 
-testschema = '{"name": "John", "age"": 30, "city": "New York"}'
-print(insertSchema("nofrills.com2", "kevin223", testschema))
+# testschema = '{"name": "John", "age"": 30, "city": "New York"}'
+# print(insertSchema("nofrills.com2", "kevin223", testschema))
 #getSchemaByWebsiteAndUser("nofrills.com", "kevin")
 
 
