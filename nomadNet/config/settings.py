b21412b6537740f9109cee6aa293fa99f5ebdb30
Bhavjot <bhavjot.grewal@mail.utoronto.ca>
2024-09-14 04:23:05 -0400

    Updated log directory

--- nomadNet/config/settings.py
+++ nomadNet/config/settings.py
@@ -1,17 +1,17 @@
 import os
 
 LTE_MODEMS = [
-    {'id': 'modem1', 'device': '/dev/ttyUSB0'},
-    {'id': 'modem2', 'device': '/dev/ttyUSB1'},
-    {'id': 'modem3', 'device': '/dev/ttyUSB2'},
-    {'id': 'modem4', 'device': '/dev/ttyUSB3'},
+    {'id': 'modem1', 'interface': 'eth1'},
+    {'id': 'modem2', 'interface': 'eth2'},
+    {'id': 'modem3', 'interface': 'eth3'},
+    {'id': 'modem4', 'interface': 'eth4'},
 ]
 
 WIFI_INTERFACE = 'wlan0'
 MAX_CONNECTIONS_PER_MODEM = 50
 IP_REFRESH_INTERVAL = 3600  # 1 hour
 AUTH_TOKEN = os.environ.get('NOMADNET_AUTH_TOKEN', 'default_secret_token')
-LOG_FILE = '/var/log/nomadnet.log'
+LOG_FILE = 'log/nomadnet.log'
 
 AUTH0_DOMAIN = os.environ.get('AUTH0_DOMAIN', 'your-auth0-domain.auth0.com')
 AUTH0_API_AUDIENCE = os.environ.get('AUTH0_API_AUDIENCE', 'https://api.nomadnet.example.com')
