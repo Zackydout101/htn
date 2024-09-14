308dc99c94321975fb1708595f75cf3f4840adda
Bhavjot <bhavjot.grewal@mail.utoronto.ca>
2024-09-14 19:07:21 -0400

    new table

--- backend/app/database/query_constants.py
+++ backend/app/database/query_constants.py
@@ -4,4 +4,6 @@ PAGE_DATA_COLUMN = 'page_data'
 USER_ID_COLUMN = 'user_id'
 WEBSITE_URL_COLUMN = 'website_url'
 RESPONSE_SCHEMA_COLUMN = 'json_schema'
+API_ENDPOINTS_TABLE = 'api_endpoints'
+API_ENDPOINT_COLUMN = 'api_endpoint'
 
