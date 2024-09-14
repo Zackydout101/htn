2db1dc0ed0aa8318179a13b9351228d66a988aa3
KevinL <kkevin.li@mail.utoronto.ca>
2024-09-14 05:13:12 -0400

    Add user auth and more data functions

--- backend/app/database/page_data.py
+++ backend/app/database/page_data.py
@@ -1,19 +1,32 @@
 from supabase import create_client, Client
-import constants
+import constants, query_constants
+from typing import List
 
 url: str = constants.SUPABASE_URL
 key: str = constants.SUPABASE_KEY
 
 supabase: Client = create_client(url, key)
 
-def getPageData(website_url:str):
+def getPageDataByWebsite(website_url:str, user_id:str) -> str:
+  response = supabase.table(query_constants.PAGE_DATA_TABLE).select("*").eq(query_constants.WEBSITE_URL_COLUMN, website_url).eq(query_constants.USER_ID_COLUMN, user_id).execute()
+  print(response)
+  pageData = response[0][query_constants.PAGE_DATA_COLUMN]
+  return pageData
 
-  response = supabase.table("testPageData").select("*").eq("id", website_url).execute()
+def getPageDataForUser(user_id:str) -> List[str]:
+  response = supabase.table(query_constants.PAGE_DATA_TABLE).select("*").eq("user_id", user_id).execute()
+  pageData = []
+
+  for data in response.data:
+    pageData.append([data[query_constants.WEBSITE_URL_COLUMN], data[query_constants.PAGE_DATA_COLUMN]])
+  print(pageData)
+
+  return pageData
+
+def insertPageData(website_url:str, user_id:str, page_data:str):
+  response = supabase.table(query_constants.PAGE_DATA_TABLE).insert({query_constants.WEBSITE_URL_COLUMN:website_url, query_constants.PAGE_DATA_COLUMN: page_data, query_constants.USER_ID_COLUMN:user_id}).execute()
   print(response)
 
-def insertPageData(website_url:str, page_data:str):
-
-  response = supabase.table("testPageData").insert({"website_url":website_url, "page_data": page_data}).execute()
-  print(response)
-
-insertPageData("amazon.com", "random data")
+# insertPageData("walmart.com", "kevin", "new data")
+# insertPageData("bestbuy.com", "kevin", "new data")
+getPageDataForUser("kevin")
