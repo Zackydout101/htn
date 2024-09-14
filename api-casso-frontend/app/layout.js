8dd69cfb45a948f79c5394847912ac1f98daed19
Karolina Dubiel <62366293+dubiels@users.noreply.github.com>
2024-09-13 23:23:59 -0700

    Update layout.js

--- api-casso-frontend/app/layout.js
+++ api-casso-frontend/app/layout.js
@@ -2,18 +2,6 @@ import localFont from "next/font/local";
 import { Inter } from "next/font/google";  // Add Inter from Google Fonts
 import "./globals.css";
 
-// Load your local fonts
-const geistSans = localFont({
-  src: "./fonts/GeistVF.woff",
-  variable: "--font-geist-sans",
-  weight: "100 900",
-});
-const geistMono = localFont({
-  src: "./fonts/GeistMonoVF.woff",
-  variable: "--font-geist-mono",
-  weight: "100 900",
-});
-
 // Load Inter from Google Fonts
 const inter = Inter({
   subsets: ["latin"],
@@ -30,7 +18,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} ${inter.variable} antialiased`}
+        className={`${inter.variable} antialiased`}
       >
         {children}
       </body>
