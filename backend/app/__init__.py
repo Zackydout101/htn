1cdca34f0160e39e914c5e85294ea5e3c26a6360
Bhavjot <bhavjot.grewal@mail.utoronto.ca>
2024-09-14 20:19:18 -0400

    Fixed server final

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -4,7 +4,7 @@ from flask_cors import CORS
 app = Flask(__name__)
 CORS(app)  # This will allow all origins by default
 
-# Import routes after creating the app
+# Import routes and database modules after creating the app
 from app.routes import scraper_routes
 from app.database import page_data, schemas, user_auth, api_endpoints
 
